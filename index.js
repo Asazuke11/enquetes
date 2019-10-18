a7c4ee59d20096687f4b223731de1dbf2157f69f
Asazuke <quusan3@gmail.com>
2019-10-18 19:51:40 +0900

    ログアウトボタン追加

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 'use strict';
 const http = require('http');
 const router = require('./lib/router');
+const auth = require('http-auth');
+const basic = auth.basic(
+  { realm: 'Enquetes Area.' },
+  (username, password, callback) => {
+    callback(username === 'guest' && password === 'xaXZJQmE');
+  });
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(basic,(req, res) => {
 	router.route(req,res);
-
 }).on('error', (e) => {
 	console.error('[' + new Date() + '] Server Error', e);
 }).on('clientError', (e) => {
